5572e0a34ba439a76410afe940b9f6ba66d24c69
Besfort21 <besfort-avdyli1@hotmail.com>
2022-03-13 16:45:48 +0100

    merge js files

--- static/myscript.js
+++ static/myscript.js
@@ -28,4 +28,18 @@ window.onload = function () {
                 wrapper.parentNode.insertBefore(wrapper.nextElementSibling, wrapper);
         });
     }
+    
+    document.getElementById("download").addEventListener("click",()=>{
+        const svpdf = this.document.getElementById("save_as_pdf");
+        var opt = {
+            margin: 1,
+            filename: 'myfile.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+        html2pdf().from(svpdf).set(opt).save();
+
+    })
+
 }
